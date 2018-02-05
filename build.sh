8fcacc4ec8da8f4a3a4afd67dd0e40f9af5a3d2d
dgafka <dgafka.mail@gmail.com>
2018-02-05 21:07:51 +0100

    push to docker registry

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@
 
 docker build -t simplycodedsoftware/debian:latest Debian/Base \
 && docker build -t simplycodedsoftware/php:7.1 Debian/PHP/7.1/Fpm \
-&& docker build -t simplycodedsoftware/ci:7.1 Debian/PHP/7.1/CI
+&& docker build -t simplycodedsoftware/ci:7.1 Debian/PHP/7.1/CI \
+&& docker push simplycodedsoftware/debian:latest \
+&& docker push simplycodedsoftware/php:7.1 \
+&& docker push simplycodedsoftware/ci:7.1
