e32e25f2ac37ca57894c913db6b07f3684b23bca
dgafka <dgafka.mail@gmail.com>
2019-11-28 16:11:12 +0100

    configuration

--- Debian/PHP/7.3/Fpm/install-dependencies.sh
+++ Debian/PHP/7.3/Fpm/install-dependencies.sh
@@ -4,12 +4,12 @@ set -e
 
 rm -rf var/cache/*
 
-if [ "$CI" = "true" ]; then \
-    echo "Running as CI" \
-    && exit 0 \
-; fi
-
 if [ "$(id -u)" = "0" ] && [ "$APP_INSTALL_DEPENDENCIES" != "no" ]; then
-   echo "installing fresh dependencies" \
+   echo "Installing fresh dependencies" \
    && su deploy -c "(composer install --ignore-platform-reqs --no-interaction --optimize-autoloader)" \
+   && echo "Dependencies installed"
+elif [ "$APP_INSTALL_DEPENDENCIES" != "no" ]; then
+   composer install --ignore-platform-reqs --no-interaction --optimize-autoloader
+else
+    echo "Not installing dependencies" \
 ; fi
--- build.sh
+++ build.sh
@@ -6,4 +6,13 @@ docker build -t simplycodedsoftware/debian:latest Debian/Base \
 && docker build -t simplycodedsoftware/ci:7.3 Debian/PHP/7.3/CI \
 && docker build -t simplycodedsoftware/nginx:backend Debian/Nginx/backend \
 && docker build -t simplycodedsoftware/nginx:frontend Debian/Nginx/frontend \
-&& docker build -t simplycodedsoftware/nginx:php-7.3 Debian/PHP/7.3/Fpm-Nginx
+&& docker build -t simplycodedsoftware/nginx:php-7.3 Debian/PHP/7.3/Fpm-Nginx \
+&& docker build -t simplycodedsoftware/npm:latest Debian/Node/Latest
+#\
+#&& docker push simplycodedsoftware/debian:latest \
+#&& docker push simplycodedsoftware/php:7.3 \
+#&& docker push simplycodedsoftware/ci:7.3 \
+#&& docker push simplycodedsoftware/nginx:backend \
+#&& docker push simplycodedsoftware/nginx:frontend \
+#&& docker push simplycodedsoftware/nginx:php-7.3 \
+#&& docker push simplycodedsoftware/npm:latest
