85c9f83ff5c76e647c05817211ed431ac01de710
dgafka <dgafka.mail@gmail.com>
2021-03-07 18:42:32 +0100

    Prepare nginx and fpm only if possible

--- Debian/PHP/8.0/Fpm-Nginx/prepare-nginx.sh
+++ Debian/PHP/8.0/Fpm-Nginx/prepare-nginx.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mkdir -p /var/lib/nginx/body /var/log/nginx && touch /var/log/nginx/error.log /var/log/nginx/app_error.log /var/log/nginx/app_access.log
+if [ "$(id -u)" = "0" ]; mkdir -p /var/lib/nginx/body /var/log/nginx && touch /var/log/nginx/error.log /var/log/nginx/app_error.log /var/log/nginx/app_access.log; fi
