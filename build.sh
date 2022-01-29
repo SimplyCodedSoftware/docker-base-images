8c87fffc6de0852d4569a3bc803ac4b6d499af62
dgafka <dgafka.mail@gmail.com>
2022-01-29 11:17:28 +0100

    php 8.1

--- build.sh
+++ build.sh
@@ -7,14 +7,11 @@ docker build --pull -t simplycodedsoftware/debian:latest Debian/Base \
 && docker push simplycodedsoftware/php:8.0 \
 && docker build --pull -t simplycodedsoftware/nginx-php:8.0 Debian/PHP/8.0/Fpm-Nginx \
 && docker push simplycodedsoftware/nginx-php:8.0
-#docker build --pull -t simplycodedsoftware/php:8.1 Debian/PHP/8.1/Fpm \
-#&& docker push simplycodedsoftware/php:8.1 \
-#&& docker build --pull -t simplycodedsoftware/nginx-php:8.1 Debian/PHP/8.1/Fpm-Nginx \
-#&& docker push simplycodedsoftware/nginx-php:8.1
-#&& docker build --pull -t simplycodedsoftware/nginx:frontend Debian/Nginx/frontend \
-#&& docker build --pull -t simplycodedsoftware/npm:latest Debian/Node/Latest \
-#&& docker push simplycodedsoftware/debian:latest \
-#&& docker push simplycodedsoftware/php:8.0 \
-#&& docker push simplycodedsoftware/nginx:php-8.0 \
-#&& docker push simplycodedsoftware/nginx:frontend \
-#&& docker push simplycodedsoftware/npm:latest
+docker build --pull -t simplycodedsoftware/php:8.1 Debian/PHP/8.1/Fpm \
+&& docker push simplycodedsoftware/php:8.1 \
+&& docker build --pull -t simplycodedsoftware/nginx-php:8.1 Debian/PHP/8.1/Fpm-Nginx \
+&& docker push simplycodedsoftware/nginx-php:8.1
+&& docker build --pull -t simplycodedsoftware/nginx:frontend Debian/Nginx/frontend \
+&& docker push simplycodedsoftware/nginx:frontend \
+&& docker build --pull -t simplycodedsoftware/npm:latest Debian/Node/Latest \
+&& docker push simplycodedsoftware/npm:latest
