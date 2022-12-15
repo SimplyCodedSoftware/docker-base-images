603dc25dff3ffee0e325ed66c9952196af2f50a4
Dariusz Gafka <dariuszgafka@gmail.com>
2022-12-15 08:11:14 +0100

    xdebug

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-docker build --pull --no-cache -t simplycodedsoftware/debian:latest Debian/Base \
+docker build --pull -t simplycodedsoftware/debian:latest Debian/Base \
 && docker push simplycodedsoftware/debian:latest \
 && docker build -t simplycodedsoftware/php:8.0 Debian/PHP/8.0/Fpm \
 && docker push simplycodedsoftware/php:8.0 \
@@ -10,8 +10,8 @@ docker build --pull --no-cache -t simplycodedsoftware/debian:latest Debian/Base 
 docker build -t simplycodedsoftware/php:8.1 Debian/PHP/8.1/Fpm \
 && docker push simplycodedsoftware/php:8.1 \
 && docker build -t simplycodedsoftware/nginx-php:8.1 Debian/PHP/8.1/Fpm-Nginx \
-&& docker push simplycodedsoftware/nginx-php:8.1
-&& docker build -t simplycodedsoftware/nginx:frontend Debian/Nginx/frontend \
+&& docker push simplycodedsoftware/nginx-php:8.1 \
+&& docker build --pull -t simplycodedsoftware/nginx:frontend Debian/Nginx/frontend \
 && docker push simplycodedsoftware/nginx:frontend \
-&& docker build -t simplycodedsoftware/npm:latest Debian/Node/Latest \
+&& docker build --pull -t simplycodedsoftware/npm:latest Debian/Node/Latest \
 && docker push simplycodedsoftware/npm:latest
