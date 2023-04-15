ac4674c1ffad4913a6e15064ec79db304957079d
Dariusz Gafka <dariuszgafka@gmail.com>
2023-04-15 14:19:25 +0200

    auto install for non root user

--- Debian/PHP/8.0/Fpm/install-dependencies.sh
+++ Debian/PHP/8.0/Fpm/install-dependencies.sh
@@ -8,6 +8,10 @@ if [ "$(id -u)" = "0" ] && [ "$APP_INSTALL_DEPENDENCIES" = "yes" ]; then
    echo "Installing fresh dependencies" \
    && su deploy -c "(composer install --ignore-platform-reqs --no-interaction --optimize-autoloader)" \
    && echo "Dependencies installed"
+elif [ "$APP_INSTALL_DEPENDENCIES" = "yes" ]; then
+    echo "Installing fresh dependencies" \
+    && composer install --ignore-platform-reqs --no-interaction --optimize-autoloader \
+    && echo "Dependencies installed"
 else
     echo "Not installing dependencies" \
 ; fi
--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-#docker build --pull -t simplycodedsoftware/debian:latest Debian/Base \
-#&& docker push simplycodedsoftware/debian:latest \
-#&& docker build -t simplycodedsoftware/php:8.0 Debian/PHP/8.0/Fpm \
-#&& docker push simplycodedsoftware/php:8.0 \
-#&& docker build -t simplycodedsoftware/nginx-php:8.0 Debian/PHP/8.0/Fpm-Nginx \
-#&& docker push simplycodedsoftware/nginx-php:8.0 \
-docker build -t simplycodedsoftware/php:8.1 Debian/PHP/8.1/Fpm \
+docker build --pull -t simplycodedsoftware/debian:latest Debian/Base \
+&& docker push simplycodedsoftware/debian:latest \
+&& docker build -t simplycodedsoftware/php:8.0 Debian/PHP/8.0/Fpm \
+&& docker push simplycodedsoftware/php:8.0 \
+&& docker build -t simplycodedsoftware/nginx-php:8.0 Debian/PHP/8.0/Fpm-Nginx \
+&& docker push simplycodedsoftware/nginx-php:8.0 \
+&& docker build -t simplycodedsoftware/php:8.1 Debian/PHP/8.1/Fpm \
 && docker push simplycodedsoftware/php:8.1 \
 && docker build -t simplycodedsoftware/nginx-php:8.1 Debian/PHP/8.1/Fpm-Nginx \
 && docker push simplycodedsoftware/nginx-php:8.1 \
