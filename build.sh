59c7e85921ce078d7a6d2fa6c2921cd279d74b6f
Dariusz Gafka <dariuszgafka@gmail.com>
2023-11-04 20:57:58 +0100

    build correct 8.2

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # build and local locally
-#docker buildx build --platform linux/amd64 --load -t simplycodedsoftware/php:8.1.25 Debian/PHP/8.1/Fpm \
-#&&  docker buildx build --platform linux/amd64 --load -t simplycodedsoftware/nginx-php:8.1.25 Debian/PHP/8.1/Fpm-Nginx \
-#&&  docker buildx build --platform linux/amd64 --load -t simplycodedsoftware/php:8.2.12 Debian/PHP/8.2/Fpm \
-#&&  docker buildx build --platform linux/amd64 --load -t simplycodedsoftware/nginx-php:8.2.12 Debian/PHP/8.2/Fpm-Nginx
+#docker build -t simplycodedsoftware/php:8.1.27 Debian/PHP/8.1/Fpm \
+#&&  docker build -t simplycodedsoftware/nginx-php:8.1.27 Debian/PHP/8.1/Fpm-Nginx \
+#&& docker build -t simplycodedsoftware/php:8.2.27 Debian/PHP/8.2/Fpm \
+#&&  docker build -t simplycodedsoftware/nginx-php:8.2.27 Debian/PHP/8.2/Fpm-Nginx
 
 # build and push
-docker buildx build --platform linux/amd64,linux/arm64 --push -t simplycodedsoftware/php:8.1.25 Debian/PHP/8.1/Fpm \
-&&  docker buildx build --platform linux/amd64,linux/arm64 --push -t simplycodedsoftware/nginx-php:8.1.25 Debian/PHP/8.1/Fpm-Nginx \
-&&  docker buildx build --platform linux/amd64,linux/arm64 --push -t simplycodedsoftware/php:8.2.12 Debian/PHP/8.2/Fpm \
-&&  docker buildx build --platform linux/amd64,linux/arm64 --push -t simplycodedsoftware/nginx-php:8.2.12 Debian/PHP/8.2/Fpm-Nginx
+docker buildx build --platform linux/amd64,linux/arm64 --push -t simplycodedsoftware/php:8.1.27 Debian/PHP/8.1/Fpm \
+&&  docker buildx build --platform linux/amd64,linux/arm64 --push -t simplycodedsoftware/nginx-php:8.1.27 Debian/PHP/8.1/Fpm-Nginx \
+&&  docker buildx build --platform linux/amd64,linux/arm64 --push -t simplycodedsoftware/php:8.2.27 Debian/PHP/8.2/Fpm \
+&&  docker buildx build --platform linux/amd64,linux/arm64 --push -t simplycodedsoftware/nginx-php:8.2.27 Debian/PHP/8.2/Fpm-Nginx
